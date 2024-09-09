9a7b8cdeca7c8555300bb9353f721f215c23f8cb
christophegauth <christophegauthiersokpoly@gmail.com>
2024-09-09 12:31:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
+import Intro from "./components/Intro/intro";
+import Navbar from "./components/Navbar/navbar";
+import Skills from "./components/Skills/skills";
+import Works from "./components/Works/works";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Intro />
+      <Skills />
+      <Works />
+      <Contact />
+      <Footer />   mishono@cliraptogo.org @Clir24#
+    </div>
+  );
+}
+
+export default App;
